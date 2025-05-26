65671562cf6971d8de3ae613241fb4ccd804b82a
Edoardo Guido <edoardo.guido.93@gmail.com>
2025-05-26 17:39:27 +0200

    feat: add example for configurable and responsive animations

--- test/src/app/page.tsx
+++ test/src/app/page.tsx
@@ -2,6 +2,31 @@
 
 import Dialogo from 'dialogo';
 import modal from 'dialogo/state';
+import { motion, Variants } from 'motion/react';
+
+const MODAL_VARIANTS: Variants = {
+  initial: {
+    opacity: 'var(--opacity-from)',
+    // scale: 'var(--scale-from)',
+    y: 'var(--y-from)',
+    // filter: 'blur(6px)',
+    transition: { type: 'spring', stiffness: 360, damping: 36, mass: 1 },
+  },
+  animate: {
+    opacity: 'var(--opacity-to)',
+    scale: 'var(--scale-to)',
+    y: 'var(--y-to)',
+    filter: 'blur(0px)',
+    transition: { type: 'spring', stiffness: 360, damping: 36, mass: 1 },
+  },
+  exit: {
+    opacity: 'var(--opacity-from)',
+    scale: 'var(--scale-from)',
+    y: 'var(--y-from)',
+    filter: 'blur(6px)',
+    transition: { type: 'spring', stiffness: 360, damping: 36, mass: 1 },
+  },
+};
 
 export default function Home() {
   return (
@@ -14,14 +39,14 @@ export default function Home() {
       >
         Open modal
       </button>
-      <Dialogo />
+      <Dialogo viewTransitionMode="popLayout" modalVariants={MODAL_VARIANTS} />
     </div>
   );
 }
 
 const FirstModalView = () => {
   return (
-    <div className="p-4 w-[480px] h-[480px]">
+    <div className="p-4 w-[480px] h-[380px]">
       <div>Ciaone</div>
       <button
         className="bg-black text-white px-2 py-0.5 cursor-pointer rounded-lg"
@@ -38,6 +63,7 @@ const FirstModalView = () => {
 const SecondModalView = () => {
   return (
     <div className="p-4 w-[480px] h-[520px]">
+      <PermanentElement activeStep={1} />
       <div>Second view</div>
       <button
         className="bg-white text-black px-2 py-0.5 cursor-pointer rounded-lg"
@@ -61,7 +87,8 @@ const SecondModalView = () => {
 
 const ThirdModalView = () => {
   return (
-    <div className="p-4 w-[480px] h-[520px]">
+    <div className="p-4 w-[420px] h-[420px]">
+      <PermanentElement activeStep={2} />
       <div>Third view</div>
       <button
         className="bg-black text-white px-2 py-0.5 cursor-pointer rounded-lg"
@@ -74,3 +101,17 @@ const ThirdModalView = () => {
     </div>
   );
 };
+
+const PermanentElement = ({ activeStep = 0 }) => {
+  return (
+    <motion.div layoutId="permanent" className="flex gap-8 items-baseline">
+      {STEPS.map((step, i) => (
+        <motion.div key={i} style={{ opacity: step === activeStep ? 1 : 0.5 }}>
+          Step <motion.span>{step}</motion.span>
+        </motion.div>
+      ))}
+    </motion.div>
+  );
+};
+
+const STEPS = [1, 2, 3];
